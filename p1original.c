adb9a25070e0532d008fc26d9812713368413a2d
Vidhyaganesh600 <97090593+Vidhyaganesh600@users.noreply.github.com>
2022-02-02 04:19:40 +0000

    p1original.c

--- p1original.c
+++ p1original.c
@@ -1,2 +1,22 @@
-
+#include<stdio.h>
+void input(int *a,int *b)
+{
+ printf("enter any two number\n");
+ scanf("%d%d",a,b);
+}
+void add(int a,int b,int *sum)
+{
+  *sum=a+b;
+}
+void output(int a,int b,int sum)
+{
+  printf("addition is :%d\n",sum);
+}
+int main()
+{
+  int x,y,z;
+  input(&x,&y);
+  add(x,y,&z);
+  output(x,y,z);
+  return 0;
 }
